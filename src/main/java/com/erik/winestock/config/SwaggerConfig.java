d298f27a02b032595c0937d40a1cc9fd38257699
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-13 15:31:19 -0300

    Adicionando as configurações do Swagger

---
+++ src/main/java/com/erik/winestock/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.erik.winestock.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.erik.winestock.controller";
+    private static final String API_TITLE = "Wine Stock API";
+    private static final String API_DESCRIPTION = "REST API for wine stock management";
+    private static final String CONTACT_NAME = "Erik Moreira";
+    private static final String CONTACT_GITHUB = "https://gtihub.com/wine";
+    private static final String CONTACT_EMAIL = "wine@hotmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version("1.0.0")
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+}
