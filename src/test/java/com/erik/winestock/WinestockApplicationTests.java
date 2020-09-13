01eda5cedfb8c8046136dbab384d2b43c6e8757d
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-13 11:49:44 -0300

    Criando o projeto

---
+++ src/test/java/com/erik/winestock/WinestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik.winestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
