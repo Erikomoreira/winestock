9d804c657669ca1951d8acd0bd2cc546331cd8a3
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-25 22:23:06 -0300

    Atualizando o controller, docs e service

--- src/main/java/com/erik/winestock/controller/WineController.java
+++ src/main/java/com/erik/winestock/controller/WineController.java
@@ -1,4 +1,51 @@
 package com.erik.winestock.controller;
 
-public class WineController {
+
+import com.erik.winestock.dto.QuantityDTO;
+import com.erik.winestock.dto.WineDTO;
+import com.erik.winestock.exception.WineAlreadyRegisteredException;
+import com.erik.winestock.exception.WineNotFoundException;
+import com.erik.winestock.exception.WineStockExceededException;
+import com.erik.winestock.service.WineService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/wines")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class WineController implements WineControllerDocs {
+
+    private final WineService wineService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public WineDTO createBeer(@RequestBody @Valid WineDTO wineDTO) throws WineAlreadyRegisteredException {
+        return wineService.createWine(wineDTO);
+    }
+
+    @GetMapping("/{name}")
+    public WineDTO findByName(@PathVariable String name) throws WineNotFoundException {
+        return wineService.findByName(name);
+    }
+
+    @GetMapping
+    public List<WineDTO> listBeers() {
+        return wineService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws WineNotFoundException {
+        wineService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public WineDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws WineNotFoundException, WineStockExceededException {
+        return wineService.increment(id, quantityDTO.getQuantity());
+    }
 }
--- src/main/java/com/erik/winestock/service/WineService.java
+++ src/main/java/com/erik/winestock/service/WineService.java
@@ -1,4 +1,73 @@
 package com.erik.winestock.service;
 
+import com.erik.winestock.dto.WineDTO;
+import com.erik.winestock.entity.Wine;
+import com.erik.winestock.exception.WineAlreadyRegisteredException;
+import com.erik.winestock.exception.WineNotFoundException;
+import com.erik.winestock.exception.WineStockExceededException;
+import com.erik.winestock.mapper.WineMapper;
+import com.erik.winestock.repository.WineRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class WineService {
+
+    private final WineRepository wineRepository;
+    private final WineMapper wineMapper = WineMapper.INSTANCE;
+
+    public WineDTO createWine(WineDTO wineDTO) throws WineAlreadyRegisteredException {
+        verifyIfIsAlreadyRegistered(wineDTO.getName());
+        Wine wine = wineMapper.toModel(wineDTO);
+        Wine savedWine = wineRepository.save(wine);
+        return wineMapper.toDTO(savedWine);
+    }
+
+    public WineDTO findByName(String name) throws WineNotFoundException {
+        Wine foundWine = wineRepository.findByName(name)
+                .orElseThrow(() -> new WineNotFoundException(name));
+        return wineMapper.toDTO(foundWine);
+    }
+
+    public List<WineDTO> listAll() {
+        return wineRepository.findAll()
+                .stream()
+                .map(wineMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Long id) throws WineNotFoundException {
+        verifyIfExists(id);
+        wineRepository.deleteById(id);
+    }
+
+    private void verifyIfIsAlreadyRegistered(String name) throws WineAlreadyRegisteredException {
+        Optional<Wine> optSavedBeer = wineRepository.findByName(name);
+        if (optSavedBeer.isPresent()) {
+            throw new WineAlreadyRegisteredException(name);
+        }
+    }
+
+    private Wine verifyIfExists(Long id) throws WineNotFoundException {
+        return wineRepository.findById(id)
+                .orElseThrow(() -> new WineNotFoundException(id));
+    }
+
+    public WineDTO increment(Long id, int quantityToIncrement) throws WineNotFoundException, WineStockExceededException {
+        Wine wineToIncrementStock = verifyIfExists(id);
+        int quantityAfterIncrement = quantityToIncrement + wineToIncrementStock.getQuantity();
+        if (quantityAfterIncrement <= wineToIncrementStock.getMax()) {
+            wineToIncrementStock.setQuantity(wineToIncrementStock.getQuantity() + quantityToIncrement);
+            Wine incrementedWineStock = wineRepository.save(wineToIncrementStock);
+            return wineMapper.toDTO(incrementedWineStock);
+        }
+        throw new WineStockExceededException(id, quantityToIncrement);
+    }
 }
+
